cd04615a1d9afdad82130dc4144fb0f02c4614f9
manuo1 <manuo1@free.fr>
2025-03-30 21:49:36 +0200

    Exercice9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,17 @@
-// votre code ici
+function checkAge(age) {
+    if (isNaN(age)) {
+        return "L'âge doit être un nombre.";
+    }
+
+    age = parseFloat(age);
+
+    if (age < 18) {
+        return "Vous êtes mineur.";
+    } else if (age >= 18 && age < 65) {
+        return "Vous êtes majeur.";
+    } else {
+        return "Vous êtes senior.";
+    }
+}
 
 export default checkAge;
